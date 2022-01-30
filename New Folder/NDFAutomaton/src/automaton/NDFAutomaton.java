61fb53db923f48a93d8113f42499f90f966f13dd
ChristyDun <78601929+ChristyDun@users.noreply.github.com>
2022-01-30 16:39:43 +0000

    Graph Structure
    Made a Start on creating a graph structure to build the Automaton off of.

---
+++ New Folder/NDFAutomaton/src/automaton/NDFAutomaton.java
@@ -0,0 +1,16 @@
+package automaton;
+
+/**
+ *
+ * @author Christy
+ */
+public class NDFAutomaton {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
---
+++ New Folder/NDFAutomaton/src/automaton/Vertex.java
@@ -0,0 +1,14 @@
+package automaton;
+
+/**
+ *
+ * @author Christy
+ */
+public class Vertex {
+    
+    String label;
+
+    public Vertex(String label) {
+        this.label = label;
+    }
+}
